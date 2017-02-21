ef723a8bccb5e4d1e3d9229893be4a35a21450ca
eddiechou <eddieechou@gmail.com>
2017-02-20 21:07:19 -0800

    Finished App

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -15,7 +15,6 @@ var SongQueue = Backbone.Collection.extend({
       if (this.length > 0) {
         this.playFirst();
       }
-      this.trigger('bubble', this);
     }, this);
 
     this.on('dequeue', function() {
